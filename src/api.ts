b1abbaeb20e720f93db4bbe6a2752d183e984d55
julianParrax <julianfranco.parra@gmail.com>
2023-12-05 23:52:20 -0300

    Added: 1. Interface with TypeSript created to use that as a model where we put all the info wihch we got from te API, and show it into the component. IMP: put the ApiKey in private, using .env. When I tryed to made it I had a couple of problems

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,32 @@
-const apikey: string = '6883c7a1ce3f1d23af9e2df10716dd51';
+import axios from "axios";
+
+export interface Article {
+  title: string;
+  description: string;
+  content: string;
+  url: string;
+  image: string;
+  publishedAt: string;
+  source: {
+    name: string;
+    url: string;
+  };
+}
+
+const apikey: string = "6883c7a1ce3f1d23af9e2df10716dd51"; // Using process.env to access API key
+if (!apikey) {
+  throw new Error('API_KEY not found in environment variables');
+}
+
 const url: string = `https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=${apikey}`;
+export const fetchNews = async (): Promise<Article[]> => {
+  try {
+    const response = await axios.get<{ articles: Article[] }>(url);
+    return response.data.articles;
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    return [];
+  }
+};
 
-fetch(url)
-  .then((response: Response) => {
-    return response.json();
-  })
-  .then((data: { articles: { title: string; description: string }[] }) => {
-    const articles = data.articles;
 
-    for (let i = 0; i < articles.length; i++) {
-      // articles[i].title
-      console.log("Title: " + articles[i].title);
-      // articles[i].description
-      console.log("Description: " + articles[i].description);
-      // You can replace {property} below with any of the article properties returned by the API.
-      // articles[i].{property}
-      // console.log(articles[i]['{property}']);
-
-      // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-      break;
-    }
-  });
--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,37 +1,38 @@
 import React, { useEffect, useState } from 'react';
-
-interface Article {
-  title: string;
-  description: string;
-  // ... otras propiedades de un artículo
-}
+import { fetchNews, Article } from '../api'; // Ruta al archivo NewsService
 
 const NewsComponent: React.FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
 
   useEffect(() => {
-    const apikey: string = '6883c7a1ce3f1d23af9e2df10716dd51';
-    const url: string = `https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=${apikey}`;
+    const obtenerNoticias = async () => {
+      try {
+        const noticias = await fetchNews();
+        setArticles(noticias);
+      } catch (error) {
+        console.error('Error al obtener las noticias:', error);
+      }
+    };
 
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const fetchedArticles: Article[] = data.articles;
-        setArticles(fetchedArticles);
-      });
+    obtenerNoticias();
   }, []);
 
+  // Renderiza tu componente de acuerdo a los datos de noticias (articles)
   return (
     <div>
-      <h1>News</h1>
+      <h1>Últimas noticias</h1>
       <ul>
         {articles.map((article, index) => (
           <li key={index}>
             <h3>{article.title}</h3>
             <p>{article.description}</p>
-            {/* Mostrar otras propiedades del artículo si es necesario */}
+            <p>{article.content}</p>
+            <p>{article.url}</p>
+            <img src={article.image} alt={article.image} />
+            <p>{article.publishedAt}</p>
+            <p>{article.source.name}</p>
+            <p>{article.source.url}</p>
+            {/* Renderiza otros detalles del artículo si es necesario */}
           </li>
         ))}
       </ul>
@@ -40,3 +41,4 @@ const NewsComponent: React.FC = () => {
 };
 
 export default NewsComponent;
+
